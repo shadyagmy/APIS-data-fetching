6a07e7d57b8a96fed2696d94c62c11a590c18b4c
SHADY AGMY <shadyagmy@yahoo.com>
2019-04-13 21:42:39 +0200

    Updates

---
+++ precache-manifest.eaef722b0a2837dc7a0893b2f5b7fac3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd52594fed67a4b44db",
+    "url": "/APIS-data-fetching/static/js/runtime~main.7974eab2.js"
+  },
+  {
+    "revision": "77b1af9aa67da1abe119",
+    "url": "/APIS-data-fetching/static/js/main.59b0558a.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/APIS-data-fetching/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "77b1af9aa67da1abe119",
+    "url": "/APIS-data-fetching/static/css/main.88c90963.chunk.css"
+  },
+  {
+    "revision": "340c365e2ccf29aef9579c2fdc740eb9",
+    "url": "/APIS-data-fetching/index.html"
+  }
+];
